b69fab66fdb649b234d4ae81f7f2832bcf04ca35
jingquan-wang <jq.wang1214@gmail.com>
2021-02-02 02:26:48 -0500

    modify app.py

--- webserver/app.py
+++ webserver/app.py
@@ -1,8 +1,12 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
-@app.route("/home", methods=["GET", "POST"])
+
+# @app.route("/home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
