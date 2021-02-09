673cf5fa46c47ee6973f4667d1989e645a02a461
jingquan-wang <jq.wang1214@gmail.com>
2021-02-09 07:13:16 -0500

    file upload

--- webserver/app.py
+++ webserver/app.py
@@ -1,14 +1,31 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, url_for, redirect
+from werkzeug import secure_filename
+import os
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+## uploading specs ##
+UPLOAD_FOLDER = "/tmp/"
+ALLOWED_EXTENSIONS = set(["csv"])
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
+
 
 # @app.route("/home", methods=["GET", "POST"])
 @app.route("/", methods=["GET", "POST"])
 def home():
+    ## file uploading stuff
+    if request.method == "POST":
+        file = request.files["file"]
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for("/", filename=filename))
     return render_template("base.html")
 
 
