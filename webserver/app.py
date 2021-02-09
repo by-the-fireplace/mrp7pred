79a734e31c85fa1a6f26e34f02de096c782991bc
jingquan-wang <jq.wang1214@gmail.com>
2021-02-08 23:38:12 -0500

    download files

--- webserver/app.py
+++ webserver/app.py
@@ -1,5 +1,5 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
@@ -10,3 +10,25 @@ bootstrap = Bootstrap(app)
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("base.html")
+
+
+@app.route("/positive", methods=["GET", "POST"])
+def positive():
+    with open("./data/positive.csv") as f:
+        csv = f.read()
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=positive.csv"},
+    )
+
+
+@app.route("/negative", methods=["GET", "POST"])
+def positive():
+    with open("./data/negative.csv") as f:
+        csv = f.read()
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=negative.csv"},
+    )
